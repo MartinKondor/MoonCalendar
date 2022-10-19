e3dd6caa28bd747a94b0901b9ddbb6f2010eab12
Martin Kondor <martinkondor@gmail.com>
2022-10-19 11:00:11 +0200

    cleaning

--- app/src/main/java/com/martinkondor/mooncalendar/CalendarActivity.java
+++ app/src/main/java/com/martinkondor/mooncalendar/CalendarActivity.java
@@ -1,6 +1,9 @@
 package com.martinkondor.mooncalendar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
@@ -9,6 +12,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.ArrayMap;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
